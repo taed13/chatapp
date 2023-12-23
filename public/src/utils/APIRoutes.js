59ffc3d689185157b1d65e5edd7398bc3658a16e
taed13 <taedtech13@gmail.com>
2023-12-23 21:20:48 +0700

    deploy vercel

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,2 +1,3 @@
-const host = "http://localhost:1337";
+// const host = "http://localhost:1337";
+const host = "https://chatapp-server-one.vercel.app";
 export const registerRoute = `${host}/api/auth/register`;
