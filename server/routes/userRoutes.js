38f6cd29ca8e36f91b0663658bd9f095a96686d3
taed13 <taedtech13@gmail.com>
2023-12-24 16:47:16 +0700

    change api method to login

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,6 @@ const { register, login } = require("../controllers/usersController");
 const router = require("express").Router();
 
 router.post("/register", register);
-router.get("/login", login);
+router.post("/login", login);
 
 module.exports = router;
